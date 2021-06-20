ea410f21442d96a65a9f449aa2bc429638289578
Netesh <48326144+netesh5@users.noreply.github.com>
2021-06-20 22:15:09 +0545

    Update main game.cpp

--- Car Game with C/main game.cpp
+++ Car Game with C/main game.cpp
@@ -10,8 +10,6 @@ int main()
     closegraph();
     return 0;
 }
-
-
 void loading()
 {
     int gd=DETECT,gm,x,y,i,n;
@@ -21,95 +19,105 @@ void loading()
     settextstyle(4,HORIZ_DIR,4);
     outtextxy(155,205,"NPPK Presents");delay(2000);
     for(i=0;i<=300;i=i+10)
-{ settextstyle(4,HORIZ_DIR,4);
-    outtextxy(155,205-i,"NPPK Presents");
-  delay(80);
-  cleardevice();
-}
-  for(i=0;i<=100;i=i+10)
-{ int border_color = WHITE;
-  setfillstyle(SOLID_FILL,RED);
-  circle(x,y-i,30);
-  floodfill(x,y-i,border_color);
-  delay(50);
-  cleardevice();
-}
-for(i=0;i<=100;i=i+10)
-{ int border_color = WHITE;
-  setfillstyle(SOLID_FILL,BLUE);
-  circle(x,y-100+i,30);
-  floodfill(x,y-100+i,border_color);
-  delay(50);
-  cleardevice();
-}
-for(i=0;i<=100;i=i+10)
-{ int border_color = WHITE;
-  setfillstyle(SOLID_FILL,GREEN);
-  circle(x,y+i,30);
-  floodfill(x,y+i,border_color);
-  delay(50);
-  cleardevice();
-}
-for(i=0;i<=100;i=i+10)
-{ int border_color = WHITE;
-  setfillstyle(SOLID_FILL,YELLOW);
-  circle(x,y+100-i,30);
-  floodfill(x,y+100-i,border_color);
-  delay(50);
-  cleardevice();
-}
-for(i=0;i<=100;i=i+10)
-{ int border_color = WHITE;
-  setfillstyle(HATCH_FILL,BROWN);
-  circle(x+i,y,30);
-  floodfill(x+i,y,border_color);
-  delay(50);
-  cleardevice();
-}
-for(i=0;i<=100;i=i+10)
-{ int border_color = WHITE;
-  setfillstyle(HATCH_FILL,MAGENTA);
-  circle(x+100-i,y,30);
-  floodfill(x+100-i,y,border_color);
-  delay(50);
-  cleardevice();
-}
-for(i=0;i<=100;i=i+10)
-{ int border_color = WHITE;
-  setfillstyle(HATCH_FILL,RED);
-  circle(x-i,y,30);
-  floodfill(x-i,y,border_color);
-  delay(50);
-  cleardevice();
-}
-for(i=0;i<=100;i=i+10)
-{ int border_color = WHITE;
-  setfillstyle(HATCH_FILL,RED);
-  circle(x-100+i,y,30);
-  floodfill(x-100+i,y,border_color);
-  delay(50);
-  cleardevice();
-}
-int border_color = WHITE;
-setfillstyle(HATCH_FILL,RED);
-circle(x,y,30);
-floodfill(x,y,border_color);
-delay(100);
-cleardevice();
-/*crashing circle*/
-for(i=0;i<=250;i=i+10)
-{int border_color = WHITE;
-  setfillstyle(SOLID_FILL,BLUE);
-  circle(x+i,y-i,30);
-  floodfill(x+i,y-i,border_color);
-  delay(50);
-  cleardevice();
-}// CAR GAME LOADING
-for(i=1;i<=7;i=i+1)
+    { 
+        settextstyle(4,HORIZ_DIR,4);
+        outtextxy(155,205-i,"NPPK Presents");
+        delay(80);
+        cleardevice();
+    }
+    for(i=0;i<=100;i=i+10)
+    { 
+        int border_color = WHITE;
+        setfillstyle(SOLID_FILL,RED);
+        circle(x,y-i,30);
+        floodfill(x,y-i,border_color);
+        delay(50);
+        cleardevice();
+    }
+    for(i=0;i<=100;i=i+10)
+    { 
+        int border_color = WHITE;
+        setfillstyle(SOLID_FILL,BLUE);
+        circle(x,y-100+i,30);
+        floodfill(x,y-100+i,border_color);
+        delay(50);
+        cleardevice();
+    }
+    for(i=0;i<=100;i=i+10)
+    { 
+        int border_color = WHITE;
+        setfillstyle(SOLID_FILL,GREEN);
+        circle(x,y+i,30);
+        floodfill(x,y+i,border_color);
+        delay(50);
+        cleardevice();
+    }
+    for(i=0;i<=100;i=i+10)
+    { 
+        int border_color = WHITE;
+        setfillstyle(SOLID_FILL,YELLOW);
+        circle(x,y+100-i,30);
+        floodfill(x,y+100-i,border_color);
+        delay(50);
+        cleardevice();
+    }
+    for(i=0;i<=100;i=i+10)
+    { 
+          int border_color = WHITE;
+          setfillstyle(HATCH_FILL,BROWN);
+          circle(x+i,y,30);
+          floodfill(x+i,y,border_color);
+          delay(50);
+          cleardevice();
+    }
+    for(i=0;i<=100;i=i+10)
+    { 
+          int border_color = WHITE;
+          setfillstyle(HATCH_FILL,MAGENTA);
+          circle(x+100-i,y,30);
+          floodfill(x+100-i,y,border_color);
+          delay(50);
+          cleardevice();
+    }
+    for(i=0;i<=100;i=i+10)
+    { 
+          int border_color = WHITE;
+          setfillstyle(HATCH_FILL,RED);
+          circle(x-i,y,30);
+          floodfill(x-i,y,border_color);
+          delay(50);
+          cleardevice();
+    }
+    for(i=0;i<=100;i=i+10)
+    { 
+          int border_color = WHITE;
+          setfillstyle(HATCH_FILL,RED);
+          circle(x-100+i,y,30);
+          floodfill(x-100+i,y,border_color);
+          delay(50);
+          cleardevice();
+    }
+    int border_color = WHITE;
+    setfillstyle(HATCH_FILL,RED);
+    circle(x,y,30);
+    floodfill(x,y,border_color);
+    delay(100);
+    cleardevice();
+    /*crashing circle*/
+    for(i=0;i<=250;i=i+10)
     {
-     setcolor(i);
-    settextstyle(3,HORIZ_DIR,1+i);
-    outtextxy(140,50,"CAR GAME");delay(700);
+          int border_color = WHITE;
+          setfillstyle(SOLID_FILL,BLUE);
+          circle(x+i,y-i,30);
+          floodfill(x+i,y-i,border_color);
+          delay(50);
+          cleardevice();
+    }// CAR GAME LOADING
+    for(i=1;i<=7;i=i+1)
+    {
+        setcolor(i);
+        settextstyle(3,HORIZ_DIR,1+i);
+        outtextxy(140,50,"CAR GAME");delay(700);
     }
     line(x-70,y,x+70,y);//car graph
     line(x-70,y,x+70,y);delay(100);
@@ -140,7 +148,7 @@ for(i=1;i<=7;i=i+1)
         rectangle(300,280,300+i,298);
         delay(15);
     }
-  for(int i=1;i<14;i++)
+    for(int i=1;i<14;i++)
     {
       settextstyle(3,HORIZ_DIR,4);
       setcolor(i-1);
